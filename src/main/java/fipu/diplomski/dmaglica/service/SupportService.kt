1f57526f66199a21d2c2e96145e49ae6de775e09
dmaglica <60017346+DavidMaglica@users.noreply.github.com>
2025-05-22 18:25:22 +0200

    damn forgor

--- src/main/java/fipu/diplomski/dmaglica/service/SupportService.kt
+++ src/main/java/fipu/diplomski/dmaglica/service/SupportService.kt
@@ -2,7 +2,6 @@ package fipu.diplomski.dmaglica.service
 
 import fipu.diplomski.dmaglica.configuration.MailSenderConfiguration
 import fipu.diplomski.dmaglica.model.response.BasicResponse
-import io.klogging.Klogging
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
@@ -13,22 +12,16 @@ class SupportService(
     private val mailSenderConfiguration: MailSenderConfiguration
 ) {
 
-    companion object : Klogging
-
-    suspend fun sendEmail(receiver: String, subject: String, body: String): BasicResponse {
-
-        val message = SimpleMailMessage().apply {
-            from = receiver
-            setTo(mailSenderConfiguration.getUsername())
-            setSubject(subject)
-            text = body
-        }
+    fun sendEmail(userEmail: String, subject: String, body: String): BasicResponse {
+        val message = SimpleMailMessage()
+        message.setTo(mailSenderConfiguration.getUsername())
+        message.subject = "Support Ticket from $userEmail - $subject"
+        message.text = body
 
         return try {
             mailSender.send(message)
             BasicResponse(true, "Email sent successfully")
         } catch (e: Exception) {
-            logger.e("Error while sending email: ${e.message}")
             return BasicResponse(false, "There was an error while sending the email")
         }
     }
